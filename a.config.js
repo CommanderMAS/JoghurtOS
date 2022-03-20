961c28780af7a5afeadca498cced48c6962f5650
CommanderMAS <38735959+CommanderMAS@users.noreply.github.com>
2022-03-20 21:49:02 +0100

    Startpoint

---
+++ a.config.js
@@ -0,0 +1,30 @@
+/** @param {NS} ns **/
+export const reservedHomeRam = 75;
+export const loop_time = 15000;
+
+//Possible Strategies
+// Hacking / Stockmarket / Crime / Corporations
+export const strategy = HackSemaphores;
+
+//Number of sleeves
+export const hassleeves = 0;
+
+//Factionwork
+
+export const factionwork = 1;
+
+//Max Augments
+
+export const trymaxaugments = 0;
+
+//END ON NEXT Cycle //Sell all Stock etc.
+export const endnow = 0;
+
+//Automatisch augmenten
+
+export const autoaug = 0;
+
+//Programme erzeugen
+//Mögliche Werte : Create / Sleeve / Buy
+
+export const get_prog = create;
