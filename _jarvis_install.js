8c16d358f32005a23889616e2f9328ab3c2f5dbf
CommanderMAS <38735959+CommanderMAS@users.noreply.github.com>
2022-03-20 21:53:33 +0100

    Update _jarvis_install.js

--- _jarvis_install.js
+++ _jarvis_install.js
@@ -1,4 +1,4 @@
 /** @param {NS} ns **/
 export async function main(ns) {
-	await ns.wget("https://raw.githubusercontent.com/trhr/phoenix-os-bitburner/main/bin.gr.future.js","bin.gr.future.js");
+	await ns.wget("https://raw.githubusercontent.com/CommanderMAS/JoghurtOS/main/a.config.js","a.test.js");
 }
