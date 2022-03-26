5f287a4f2a878f35e95f0a8b26d89e02e55fb52f
CommanderMAS <38735959+CommanderMAS@users.noreply.github.com>
2022-03-26 09:00:03 +0100

    Update _update_joghurt.js

--- _update_joghurt.js
+++ _update_joghurt.js
@@ -1,8 +1,8 @@
 export async function main(ns) {
 
     await ns.wget("https://raw.githubusercontent.com/CommanderMAS/JoghurtOS/main/_joghurt_install.js","_joghurt_install.js");
-    ns.exec('_trash_joghurt.js','home',1,"update");
     if (ns.args[0]!="install") {
+        ns.exec('_trash_joghurt.js','home',1,"update");
         ns.tprint('Not Install');
         //ns.exec('_joghurt_install.js', 'home', 1);
     }
